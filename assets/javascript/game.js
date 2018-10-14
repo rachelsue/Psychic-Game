9849a8ee5d7a8d8e2e44dc4d26221b857a704c34
Rachel Wells <rachelwells@cpe-172-100-2-179.twcny.res.rr.com>
2018-10-14 16:21:38 -0500

    near complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,31 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var guessesDone = "";
-var cpuChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var guessesDone =[];
+var cpuChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 
 
 document.onkeyup = function(event) {
 
     var userChoice = event.key;
-
+    
+    
+    guessesDone.push(" " + userChoice);
    
     var answer = cpuChoices[Math.floor(Math.random() * cpuChoices.length)];
     
-    for (i = 1; i <10; ++i) {
-        guessesDone(parseInt(-1));
-        
 
-if (answer === userChoice) {
-    wins++;
-}   else {
-    losses++;
+if (answer === userChoice.toLowerCase()) {
+    wins++; guessesDone = [];
+}   if (answer !== userChoice) {
+    guessesLeft = guessesLeft-1;
+}   
+if  (guessesLeft <0) {
+    guessesLeft =10; losses++; guessesDone = [];
 }
+
+
 
 
 
@@ -34,14 +39,8 @@ var html =
 "<p>wins: " + wins + "</p>" +
 "<p>losses: " + losses + "</p>" +
 "<p> Guesses Left: " +guessesLeft+ "</p>"+
-",p> Guesses Done: " + "</p>";
+"<p> Guesses Done: " +guessesDone+"</p>";
 
 document.querySelector("#game").innerHTML = html;
 
- }
-
-
-
-
-
-
+}
